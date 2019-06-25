8f38596282b40eca4a194f6d3ce03ed8c8bc99c3
lee <lee.unseo17@gmail.com>
2019-06-25 15:01:44 +0900

    add merge sort

--- test.lisp
+++ test.lisp
@@ -2,6 +2,13 @@
   (format t "Welcome to the world of CommonLisp!"))
 
 
+(defun randlist (n)
+  (let ((lst ()))
+    (dotimes (i n)
+      (setf lst (cons (random 101) lst)))
+    lst))
+
+
 (defun insertion (n lst)
   (if lst
       (if (> n (car lst))
@@ -38,18 +45,11 @@
     ((<= (car l) (car r)) (cons (car l) (merge-lr (cdr l) r)))
     ((<  (car r) (car l)) (cons (car r) (merge-lr l (cdr r))))))
 
-(defun split-half ())
-
 (defun m-sort (lst)
   (if (cdr lst)
       (merge-lr 
-        (m-sort (values (subseq lst 0 (floor (length lst)))))
-        (m-sort (values (subseq lst (floor (length lst)) (length lst)))))
+        (m-sort (values (subseq lst 0 (floor (length lst) 2))))
+        (m-sort (values (subseq lst (floor (length lst) 2) (length lst)))))
       lst))
-
-
-
-
-
 
 
